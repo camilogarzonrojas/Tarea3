6c5be287399819f9c1949b2c9b896f09086c8281
camilogarzonrojas <ce.garzon12@uniandes.edu.co>
2018-10-30 23:52:08 -0500

    Avance de PCA

--- PCR.py
+++ PCR.py
@@ -54,13 +54,13 @@ print("Segun el componente principal 2, la variable mas importante es la columna
 n=len(Cov)
 indiceCP1=0 #Este es el indice de la primer componente principal
 for i in range(n):
-    if EIG[0][i] > EIG[0][indiceCP1]:
-        indiceCP1 = i
+    if EIG[0][i]>EIG[0][indiceCP1]:
+        indiceCP1=i
         
 indiceCP2 = 1 #Este el el indice de la segunda componente principal
 for i in range(n):
-    if EIG[0][i] > EIG[0][indiceCP2] and EIG[0][i] < EIG[0][indiceCP1]: #Se establece la condicion de quesea mayor a los otros y menor que la componente 1
-        indiceCP2 = i
+    if EIG[0][i]>EIG[0][indiceCP2] and EIG[0][i]<EIG[0][indiceCP1]: #Se establece la condicion de quesea mayor a los otros y menor que la componente 1
+        indiceCP2=i
 
 # Se extraen los autovectores de los autovalores mas grandes
 vector_cp1=EIG[1][:,indiceCP1]
@@ -70,3 +70,26 @@ print("Componente principal 1 ", vector_cp1)
 print("Componente principal 2 ", vector_cp2)
 print("Segun el componente principal 1, la variable mas importante es la columna", 2+np.argmax(np.abs(vector_cp1)), "del archivo de texto")
 print("Segun el componente principal 2, la variable mas importante es la columna", 2+np.argmax(np.abs(vector_cp2)), "del archivo de texto")
+
+###Quinta parte###
+#Se tienen que hallar las proyecciones
+#SE dividen los datos entre malignos y benignos
+datosM=np.array(datos.loc[ datos[1]=='M' , 2:])
+datosB=np.array(datos.loc[ datos[1]=='B' , 2:])
+
+#Se hayan las proyecciones de los datos
+proyeccionM=[np.matmul(DatosM,vector_cp1), np.matmul(DatosM,vector_cp2)]
+proyeccionB=[np.matmul(DatosB,vector_cp1), np.matmul(DatosB,vector_cp2)]
+
+
+fig=plt.figure() # SE crea una figura
+ax=fig.gca()
+ax.plot(proyeccionM[0], proyeccionM[1], '3') #SE grafica sobre los ejes
+ax.plot(proyeccionB[0], proyeccionB[1], '4')
+ax.set_xlabel('Componente 1')
+ax.set_ylabel('Componente 2')
+ax.legend(['M','B'], loc='best')
+fig.savefig('GarzonCamilo_PCA', format='pdf')
+
+###Sexta parte###
+
