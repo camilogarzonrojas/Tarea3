1570c8a73f3c317e7f91c7215912ad75b8ce36b3
camilogarzonrojas <ce.garzon12@uniandes.edu.co>
2018-10-30 23:30:36 -0500

    Avance de PCA

--- PCR.py
+++ PCR.py
@@ -17,7 +17,7 @@ mediciones=np.array(datos.iloc[:,2:])
 delta=mediciones-np.mean(mediciones,0) #Se le resta a cada columna el promedio de dicha columna. En delta quedan guardados los valores
 #Al multiplicar delta.T*delta se realizan todas las multiplicaciones [x-E(x)]*[y-E(y)] y se suman
 #Al ya estar sumados, se divide entre n-1 para encontrar la covarianza
-Cov = np.matmul(delta.T,delta)/(len(delta)-1)
+Cov=np.matmul(delta.T,delta)/(len(delta)-1)
 
 print(Cov, '\n'*3, np.cov(mediciones.T))
 
