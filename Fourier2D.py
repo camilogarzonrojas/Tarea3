967f289f1734ee14565d2e756ee9b2b78a05ca0f
camilogarzonrojas <ce.garzon12@uniandes.edu.co>
2018-10-31 19:12:21 -0500

    Avance de Fourier 2D

--- Fourier2D.py
+++ Fourier2D.py
@@ -4,5 +4,16 @@ from scipy.fftpack import ifft2, fft2
 from scipy.ndimage import imread
 
 ###Primera parte###
-arbol = imread('arbol.PNG', flatten=True)
+arbol=imread('arbol.PNG', flatten=True)
 print("Cargar imagen del arbol", np.shape(arbol)) #Se cargan los datos
+
+
+###Segunda parte###
+arbol_F=fft2(arbol)
+print("Transformada de Fourier del arbol") #Transformada de Fourier usando paquete de scipy
+
+fig,ax=plt.subplots()
+ax.imshow(np.abs(arbol_F),vmin=1000,vmax=5000)
+ax.set_xlabel(['x'])
+ax.set_ylabel(['y'])
+fig.savefig('GarzonCamilo_FT2D.pdf') #Se grafica la transformada de Fourier
