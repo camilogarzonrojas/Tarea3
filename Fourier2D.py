128e6794e565661ea7207262e5d52ed383421fba
camilogarzonrojas <ce.garzon12@uniandes.edu.co>
2018-10-31 19:34:32 -0500

    Avance de Fourier 2D

--- Fourier2D.py
+++ Fourier2D.py
@@ -17,3 +17,20 @@ ax.imshow(np.abs(arbol_F),vmin=1000,vmax=5000)
 ax.set_xlabel(['x'])
 ax.set_ylabel(['y'])
 fig.savefig('GarzonCamilo_FT2D.pdf') #Se grafica la transformada de Fourier
+
+
+###Tercera parte###
+m,n=np.shape(arbol_F)
+Filtrado = np.copy(arbol_F) #Se comienza a filtrar
+
+for i in range(m):
+    for j in range(n):
+       
+        if (0<i and i<20) and (20<j and j<40):
+            Filtrado[i,j]=0
+        if (60<i and i<80) and (60<j and j<80):
+            Filtrado[i,j]=0
+        if (180<i and i<200) and (180<j and j<200):
+            Filtrado[i,j]=0
+        if (240<i and i<250) and (220<j and j<240):
+            Filtrado[i,j]=0   #Se dejan en cero las amplitudes de las frecuencias que generan el ruido periodico haciendo un doble recorrido
